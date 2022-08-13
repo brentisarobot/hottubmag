215a2477457efcf397affe54fbab118aca5760f7
brentisarobot <brentisarobot@gmail.com>
2022-08-13 12:12:11 -0500

    conditional rendering added to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
 import './About.css'
 
-const About = () => (
-    
+const About = () => {
+
+    const [more, setMore] = useState(false)
+
+    return (    
         <>
         <main className="flex h-screen bg-yellow-200">
             <article className="container m-auto text-red-700">
@@ -12,17 +15,22 @@ const About = () => (
                     Porro consectetur quisquam animi voluptatem fugiat veniam ipsum dignissimos quia perferendis laborum quos quas tempore laudantium repudiandae, corrupti aperiam aliquid saepe esse voluptatibus dolorum consequuntur quis eos vitae. Non, inventore?
                     Adipisci impedit repellat aliquid vel reiciendis perferendis vero! Dicta, laudantium. Amet, autem provident natus voluptatum, eaque consectetur alias similique corporis ipsam quibusdam repellat modi iusto quasi iste omnis eius nihil?
                 </p>
-                <p className="mb-8">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita, error quis quo quia accusantium dignissimos, rem ex explicabo voluptatum asperiores repellendus saepe dolorum aspernatur eaque? Ad ea sequi beatae aut.
-                    Eligendi quibusdam, repellendus aliquid quisquam voluptatem quae, facere explicabo eum asperiores accusamus similique sint nulla totam id, fugit molestiae inventore autem vero distinctio. Praesentium, quaerat atque optio sapiente eaque placeat.
-                </p>
-                <p className="mb-8">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, voluptas a voluptatem odit maiores nobis porro explicabo eveniet fugiat consectetur esse saepe iure tempora ab repellendus, itaque debitis animi rem.
-                </p>
+                {more && 
+                <div>              
+                    <p className="mb-8">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita, error quis quo quia accusantium dignissimos, rem ex explicabo voluptatum asperiores repellendus saepe dolorum aspernatur eaque? Ad ea sequi beatae aut.
+                        Eligendi quibusdam, repellendus aliquid quisquam voluptatem quae, facere explicabo eum asperiores accusamus similique sint nulla totam id, fugit molestiae inventore autem vero distinctio. Praesentium, quaerat atque optio sapiente eaque placeat.
+                    </p>
+                    <p className="mb-8">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, voluptas a voluptatem odit maiores nobis porro explicabo eveniet fugiat consectetur esse saepe iure tempora ab repellendus, itaque debitis animi rem.
+                    </p>
+                </div>
+                }
+                <button onClick={()=>(setMore(!more))}>Show {more ? 'less' : 'more'}</button>
             </article>
         </main>
         </>
-    )
-
+        )
+    }
 
 export default About
